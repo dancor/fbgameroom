ea6819b85a83ce6108c8be446a0050b6a9ab5464
Daniel Corson <danl@alum.mit.edu>
2009-10-27 20:37:38 -0700

    work with .jp file crap output

--- src/shrink.hs
+++ src/shrink.hs
@@ -1,19 +1,24 @@
+import Control.Applicative
 import Control.Concurrent
 import Data.List
+import Data.Ord
 import HSH
 import System.Directory
 import System.FilePath
 
-shrink f = if ".jpg" `isSuffixOf` f
-  then do
-    runIO ("convert", ["-resize", "1x1", "pix" </> f,
-      "pixsm" </> take (length f - 3) f ++ "png"])
-    removeFile $ "pix" </> f
-  else return ()
+shrink f = do
+  runIO ("convert", ["-resize", "1x1", "pix" </> f,
+    "pixsm" </> f ++ ".png"])
+  removeFile $ "pix" </> f
+
+fixdate d = year ++ ":" ++ monthDay ++ rest where
+  (monthDay, d2) = splitAt 5 d
+  (year, rest) = splitAt 4 $ tail d2
 
 main = do
   createDirectoryIfMissing False "pixsm"
-  fs <- getDirectoryContents "pix"
-  mapM_ shrink fs
+  fs <- filter ((/= '.') . head) <$> getDirectoryContents "pix"
+  mapM_ shrink $ sortBy (comparing fixdate) fs
   threadDelay 2000000
   main
+
